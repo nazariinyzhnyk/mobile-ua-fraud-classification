cabea94a9da98a355611dd2d5582915f9e0c5b3a
Misha Zhukov <3v1l91l@gmail.com>
2018-03-03 17:25:04 +0200

    Add multi-class feature extraction and multi-class xgboost classification

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from lib import load_data
+from tqdm import tqdm
 
 config = {
     'only_click_touch_type': False
@@ -9,6 +10,7 @@ config = {
 data, data_valid, labels = load_data(valid_size=0.2)
 data['is_fraud'] = ~data['Fraud_reasons'].isnull()
 data_valid['is_fraud'] = ~data_valid['Fraud_reasons'].isnull()
+extracted_features_columnns = ['Fraud_reasons']
 
 if config['only_click_touch_type']:
     data = data[data['attributed_touch_type'] == 'click']
@@ -18,46 +20,41 @@ def set_time_features(data, data_valid):
                                - pd.to_datetime(data['attributed_touch_time'])).dt.total_seconds()
     data_valid['time_difference'] = (pd.to_datetime(data_valid['install_time'])
                                - pd.to_datetime(data_valid['attributed_touch_time'])).dt.total_seconds()
+    extracted_features_columnns.extend(['time_difference'])
+    return data, data_valid
+
+def set_fraud_rate_feature(train_df, valid_df, column, mean_fraud, fraud_reason):
+    rate_feature_column = column + '_' + fraud_reason + '_fraud_rate'
+    ratio = train_df[['Fraud_reasons', column]].groupby([column]).agg(
+        lambda x: (x['Fraud_reasons'] == fraud_reason).sum()/len(x))
+    ratio.rename(columns={'Fraud_reasons': rate_feature_column}, inplace=True)
+
+    train_df = pd.merge(train_df, ratio, how='left', left_on=column, right_index=True)
+    train_df[rate_feature_column] = train_df[rate_feature_column].fillna(mean_fraud)
+
+    valid_df = pd.merge(valid_df, ratio, how='left', left_on=column, right_index=True)
+    valid_df[rate_feature_column] = valid_df[rate_feature_column].fillna(mean_fraud)
+
+    extracted_features_columnns.extend([rate_feature_column])
+
+    return train_df, valid_df
+
+fraud_rates_columns = ['site_id', 'sub_site_id', 'publisher', 'operator', 'city', 'device_type', 'os_version', 'sdk_version']
+def set_fraud_rate_features(data, data_valid, fraud_reason):
+    mean_fraud = (data['Fraud_reasons'] == fraud_reason).mean()
+    for col in tqdm(fraud_rates_columns):
+        data, data_valid = set_fraud_rate_feature(data, data_valid, col, mean_fraud, fraud_reason)
+
     return data, data_valid
 
 data, data_valid = set_time_features(data, data_valid)
 
-def set_fraud_rate_feature(train_df, valid_df, column, mean_fraud):
-    rate_feature_column = column + '_fraud_rate'
-    site_ratio = train_df[['is_fraud', column]].groupby([column]).mean().reset_index().rename(
-        columns={'is_fraud': rate_feature_column})
-    train_df = pd.merge(train_df, site_ratio, how='left', on=column)
-    train_df[rate_feature_column] = train_df[rate_feature_column].fillna(mean_fraud)
+for fraud_reason in labels['Fraud_reasons'].unique():
+    print(f'Extracting features for fraud reason: {fraud_reason}')
+    data, data_valid = set_fraud_rate_features(data, data_valid, fraud_reason)
 
-    valid_df = pd.merge(valid_df, site_ratio, how='left', on=column)
-    valid_df[rate_feature_column] = valid_df[rate_feature_column].fillna(mean_fraud)
-    return train_df, valid_df
-
-def set_fraud_rate_features(data, data_valid, fraud_reason):
-    mean_fraud = data['is_fraud'].mean()
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'site_id', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'sub_site_id', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'publisher', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'operator', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'city', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'device_type', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'os_version', mean_fraud)
-    data, data_valid = set_fraud_rate_feature(data, data_valid, 'sdk_version', mean_fraud)
-
-    return data, data_valid
-
-for fraud_reason in labels['Fraud_reasons'].values:
-    set_fraud_rate_features(data, data_valid, fraud_reason)
-
-data, data_valid = set_fraud_rate_features(data, data_valid)
-
-
-extracted_features_train = data[['is_fraud', 'site_id_fraud_rate', 'sub_site_id_fraud_rate', 'publisher_fraud_rate',
-                      'operator_fraud_rate', 'city_fraud_rate', 'device_type_fraud_rate', 'os_version_fraud_rate',
-                      'sdk_version_fraud_rate', 'time_difference']]
+extracted_features_train = data[extracted_features_columnns]
 extracted_features_train.to_csv('extracted_features_train.csv', sep=';', index=False, encoding='utf-8')
 
-extracted_features_valid = data_valid[['is_fraud', 'site_id_fraud_rate', 'sub_site_id_fraud_rate', 'publisher_fraud_rate',
-                      'operator_fraud_rate', 'city_fraud_rate', 'device_type_fraud_rate', 'os_version_fraud_rate',
-                      'sdk_version_fraud_rate', 'time_difference']]
+extracted_features_valid = data_valid[extracted_features_columnns]
 extracted_features_valid.to_csv('extracted_features_valid.csv', sep=';', index=False, encoding='utf-8')
--- fraud_detection.py
+++ fraud_detection.py
@@ -1,68 +1,38 @@
 import pandas as pd
-# import xgboost as xgb
-# from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from catboost import Pool, CatBoostClassifier, cv, CatboostIpythonWidget
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import pickle
 
-data = pd.read_csv('data_to_model.csv', sep=';', dtype={'publisher_data': str, 'site_id': str})
+data_train = pd.read_csv('extracted_features_train.csv', sep=';')
+data_valid = pd.read_csv('extracted_features_valid.csv', sep=';')
 
-#, dtype={'os_version': str, 'device_type': str, 'fraud_flag': int, 'wifi': np.float,
-                                                  #       'time_difference': np.float}, encoding='utf-8')
-print(data.dtypes)
-target = data['fraud_flag']
-data = data.loc[:, data.columns != 'fraud_flag']
+fraud_types = np.array(['click_spamming', 'tti_fraud', 'mix', 'Data center', 'bots'])
+print(data_train.dtypes)
+label_encoder = LabelEncoder()
+data_train.loc[data_train['Fraud_reasons'].isnull(), 'Fraud_reasons'] = 'ok'
+data_valid.loc[data_valid['Fraud_reasons'].isnull(), 'Fraud_reasons'] = 'ok'
 
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=42)
-categorical_features_indices = np.where(X_train.dtypes != np.float)[0]
-print(data.columns[categorical_features_indices])
-X_train.fillna(-999, inplace=True)
-X_test.fillna(-999, inplace=True)
+le = label_encoder.fit(data_train['Fraud_reasons'].values)
+y_train = le.transform(data_train['Fraud_reasons'].values)
+y_valid = le.transform(data_valid['Fraud_reasons'].values)
+x_train = data_train.loc[:, data_train.columns != 'Fraud_reasons']
+x_valid = data_valid.loc[:, data_valid.columns != 'Fraud_reasons']
+x_valid = x_valid[x_train.columns]
 
-model = CatBoostClassifier(
-    iterations=100,
-    loss_function='Logloss',
-    random_seed=42,
-    eval_metric='Accuracy'
-)
-# model.fit(
-#     X_train, y_train,
-#     cat_features=categorical_features_indices,
-#     eval_set=(X_test, y_test),
-#     logging_level='Verbose',  # you can uncomment this for text output
-#     plot=False,
-#     use_best_model=True
-# )
-# model.save_model('model.model')
-model.load_model('model.model')
-print((model.predict(X_test) == y_test).sum()/len(y_test))
+model = XGBClassifier(max_depth=7, learning_rate=0.2, n_estimators=200,
+                      objective='multi:softmax', eval_metric='mlogloss')
+model.fit(x_train, y_train, eval_set=[(x_valid, y_valid)], early_stopping_rounds=3)
+pickle.dump(model, open("xgb.model", "wb"))
+model = pickle.load(open("xgb.model", "rb"))
 
-# cv_data = cv(
-#     model.get_params(),
-#     Pool(data.values, label=target.values, cat_features=categorical_features_indices),
-# )
-#
-# print('Best validation accuracy score: {:.2f}±{:.2f} on step {}'.format(
-#     np.max(cv_data['Accuracy_test_avg']),
-#     cv_data['Accuracy_test_stddev'][np.argmax(cv_data['Accuracy_test_avg'])],
-#     np.argmax(cv_data['Accuracy_test_avg'])
-# ))
-# print('Precise validation accuracy score: {}'.format(np.max(cv_data['Accuracy_test_avg'])))
+y_pred = model.predict(x_valid)
 
-#
-# # pd.get_dummies(X_train['device_type'], prefix='vi', prefix_sep='_')
-#
-# # model = XGBClassifier()
-# model = LGBMClassifier()
-# model.fit(X_train.values, y_train.values)
-#
-# print(model)
-#
-# y_pred = model.predict(X_test)
-# predictions = [round(value) for value in y_pred]
-#
-# accuracy = accuracy_score(y_test, predictions)
-# print("Accuracy: %.2f%%" % (accuracy * 100.0))
+accuracy = accuracy_score(y_valid, y_pred)
+precisions = precision_score(le.inverse_transform(y_valid), le.inverse_transform(y_pred), average=None, labels=le.classes_)
+recalls = recall_score(le.inverse_transform(y_valid), le.inverse_transform(y_pred), average=None, labels=le.classes_)
+print(f'Accuracy: {100 * accuracy:.2f}%')
+for i in range(len(le.classes_)):
+    print(f'Fraud reason: {le.classes_[i]}, Precision: {(100 * precisions[i]):.2f}%, Recall: {(100 * recalls[i]):.2f}%')
