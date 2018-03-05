d69ac94cf4e34fe9c3275614ef06b79c79075301
Misha Zhukov <3v1l91l@gmail.com>
2018-03-05 09:02:57 +0200

    Add feature - average across all rate features

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,59 +2,119 @@ import pandas as pd
 import numpy as np
 from lib import load_data
 from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler
 
 config = {
-    'only_click_touch_type': False
+    'only_click_touch_type': False,
+    'standard_scale': True
 }
 
 data, data_valid, labels = load_data(valid_size=0.2)
 data['is_fraud'] = ~data['Fraud_reasons'].isnull()
 data_valid['is_fraud'] = ~data_valid['Fraud_reasons'].isnull()
-extracted_features_columnns = ['Fraud_reasons']
+data['install_time'] = pd.to_datetime(data['install_time'])
+data_valid['install_time'] = pd.to_datetime(data_valid['install_time'])
+data['attributed_touch_time'] = pd.to_datetime(data['attributed_touch_time'])
+data_valid['attributed_touch_time'] = pd.to_datetime(data_valid['attributed_touch_time'])
+# data['contributor_1_touch_time'] = pd.to_datetime(data['contributor_1_touch_time'], errors='coerce')
+# data['contributor_2_touch_time'] = pd.to_datetime(data['contributor_2_touch_time'], errors='coerce')
+# data['contributor_3_touch_time'] = pd.to_datetime(data['contributor_3_touch_time'], errors='coerce')
+# data_valid['contributor_1_touch_time'] = pd.to_datetime(data_valid['contributor_1_touch_time'], errors='coerce')
+# data_valid['contributor_2_touch_time'] = pd.to_datetime(data_valid['contributor_2_touch_time'], errors='coerce')
+# data_valid['contributor_3_touch_time'] = pd.to_datetime(data_valid['contributor_3_touch_time'], errors='coerce')
 
 if config['only_click_touch_type']:
     data = data[data['attributed_touch_type'] == 'click']
 
+extracted_features_columns = np.array([])
 def set_time_features(data, data_valid):
-    data['time_difference'] = (pd.to_datetime(data['install_time'])
-                               - pd.to_datetime(data['attributed_touch_time'])).dt.total_seconds()
+    data['time_difference'] = (data['install_time']
+                               - data['attributed_touch_time']).dt.total_seconds()
     data_valid['time_difference'] = (pd.to_datetime(data_valid['install_time'])
-                               - pd.to_datetime(data_valid['attributed_touch_time'])).dt.total_seconds()
-    extracted_features_columnns.extend(['time_difference'])
-    return data, data_valid
+                               - data_valid['attributed_touch_time']).dt.total_seconds()
+    data['install_time_since_midnight_sec'] = (data['install_time'] -
+                                               pd.to_datetime(data['install_time'].dt.date)) / np.timedelta64(1, 's')
+    data_valid['install_time_since_midnight_sec'] = (data_valid['install_time'] -
+                                               pd.to_datetime(data_valid['install_time'].dt.date)) / np.timedelta64(1, 's')
+
+    data['attributed_touch_time_since_midnight_sec'] = (data['attributed_touch_time'] -
+                                               pd.to_datetime(data['attributed_touch_time'].dt.date)) / np.timedelta64(1, 's')
+    data_valid['attributed_touch_time_since_midnight_sec'] = (data_valid['attributed_touch_time'] -
+                                                        pd.to_datetime(
+                                                            data_valid['attributed_touch_time'].dt.date)) / np.timedelta64(1,
+                                                                                                                     's')
+    # data['tti_contributor1'] = (data['install_time']
+    #                             - data_valid['contributor_1_touch_time']).dt.total_seconds()
+    # data['tti_contributor2'] = (data['install_time']
+    #                             - data_valid['contributor_2_touch_time']).dt.total_seconds()
+    # data['tti_contributor3'] = (data['install_time']
+    #                             - data_valid['contributor_3_touch_time']).dt.total_seconds()
+    #
+    # data_valid['tti_contributor1'] = (data_valid['install_time']
+    #                             - data_valid['contributor_1_touch_time']).dt.total_seconds()
+    # data_valid['tti_contributor2'] = (data_valid['install_time']
+    #                             - data_valid['contributor_2_touch_time']).dt.total_seconds()
+    # data_valid['tti_contributor3'] = (data_valid['install_time']
+    #                             - data_valid['contributor_3_touch_time']).dt.total_seconds()
+    extracted_features_columns = np.array(['time_difference', 'install_time_since_midnight_sec', 'attributed_touch_time_since_midnight_sec'])
+                                        # 'tti_contributor1', 'tti_contributor2', 'tti_contributor3'])
+    return data, data_valid, extracted_features_columns
 
 def set_fraud_rate_feature(train_df, valid_df, column, mean_fraud, fraud_reason):
     rate_feature_column = column + '_' + fraud_reason + '_fraud_rate'
+    print(column)
     ratio = train_df[['Fraud_reasons', column]].groupby([column]).agg(
         lambda x: (x['Fraud_reasons'] == fraud_reason).sum()/len(x))
     ratio.rename(columns={'Fraud_reasons': rate_feature_column}, inplace=True)
 
     train_df = pd.merge(train_df, ratio, how='left', left_on=column, right_index=True)
     train_df[rate_feature_column] = train_df[rate_feature_column].fillna(mean_fraud)
+    # train_df[rate_feature_column] = train_df[rate_feature_column].fillna(0)
+
 
     valid_df = pd.merge(valid_df, ratio, how='left', left_on=column, right_index=True)
     valid_df[rate_feature_column] = valid_df[rate_feature_column].fillna(mean_fraud)
+    # valid_df[rate_feature_column] = valid_df[rate_feature_column].fillna(0)
+    return train_df, valid_df, rate_feature_column
 
-    extracted_features_columnns.extend([rate_feature_column])
-
-    return train_df, valid_df
-
-fraud_rates_columns = ['site_id', 'sub_site_id', 'publisher', 'operator', 'city', 'device_type', 'os_version', 'sdk_version']
+fraud_rates_columns = ['site_id', 'sub_site_id', 'language', 'publisher', 'operator', 'city', 'device_type',
+                       'os_version', 'sdk_version', 'app_id', 'app_version', 'country_code', 'wifi']
 def set_fraud_rate_features(data, data_valid, fraud_reason):
     mean_fraud = (data['Fraud_reasons'] == fraud_reason).mean()
+    fraud_reason_features_columns = np.array([])
     for col in tqdm(fraud_rates_columns):
-        data, data_valid = set_fraud_rate_feature(data, data_valid, col, mean_fraud, fraud_reason)
+        data, data_valid, fraud_reason_features_column = set_fraud_rate_feature(data, data_valid, col, mean_fraud,
+                                                                               fraud_reason)
+        fraud_reason_features_columns = np.concatenate((fraud_reason_features_columns, np.array([fraud_reason_features_column])))
 
-    return data, data_valid
+    return data, data_valid, fraud_reason_features_columns
 
-data, data_valid = set_time_features(data, data_valid)
+data, data_valid, time_features = set_time_features(data, data_valid)
+extracted_features_columns = np.concatenate((extracted_features_columns, time_features))
 
 for fraud_reason in labels['Fraud_reasons'].unique():
     print(f'Extracting features for fraud reason: {fraud_reason}')
-    data, data_valid = set_fraud_rate_features(data, data_valid, fraud_reason)
+    data, data_valid, fraud_reason_features_columns = set_fraud_rate_features(data, data_valid, fraud_reason)
+    data[fraud_reason + '_mean_rate'] = data[fraud_reason_features_columns].mean(axis=1)
+    data_valid[fraud_reason + '_mean_rate'] = data_valid[fraud_reason_features_columns].mean(axis=1)
+    fraud_reason_features_columns = np.concatenate((fraud_reason_features_columns, np.array([fraud_reason + '_mean_rate'])))
+    extracted_features_columns = np.concatenate((extracted_features_columns, fraud_reason_features_columns))
 
-extracted_features_train = data[extracted_features_columnns]
+if config['standard_scale']:
+    scaler = StandardScaler()
+    scaler.fit(data[extracted_features_columns])
+    data[extracted_features_columns] = scaler.transform(data[extracted_features_columns])
+    data_valid[extracted_features_columns] = scaler.transform(data_valid[extracted_features_columns])
+
+extracted_features_columns = np.concatenate((extracted_features_columns, np.array(['Fraud_reasons'])))
+extracted_features_train = data[extracted_features_columns]
 extracted_features_train.to_csv('extracted_features_train.csv', sep=';', index=False, encoding='utf-8')
 
-extracted_features_valid = data_valid[extracted_features_columnns]
+extracted_features_valid = data_valid[extracted_features_columns]
 extracted_features_valid.to_csv('extracted_features_valid.csv', sep=';', index=False, encoding='utf-8')
+
+time_features = np.concatenate((time_features, np.array(['Fraud_reasons'])))
+time_features_train = data[time_features]
+time_features_train.to_csv('time_features_train.csv', sep=';', index=False, encoding='utf-8')
+time_features_valid = data_valid[time_features]
+time_features_valid.to_csv('time_features_valid.csv', sep=';', index=False, encoding='utf-8')
--- fraud_detection.py
+++ fraud_detection.py
@@ -5,12 +5,13 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import pickle
+import operator
+import matplotlib.pyplot as plt
 
 data_train = pd.read_csv('extracted_features_train.csv', sep=';')
 data_valid = pd.read_csv('extracted_features_valid.csv', sep=';')
 
 fraud_types = np.array(['click_spamming', 'tti_fraud', 'mix', 'Data center', 'bots'])
-print(data_train.dtypes)
 label_encoder = LabelEncoder()
 data_train.loc[data_train['Fraud_reasons'].isnull(), 'Fraud_reasons'] = 'ok'
 data_valid.loc[data_valid['Fraud_reasons'].isnull(), 'Fraud_reasons'] = 'ok'
@@ -22,9 +23,12 @@ x_train = data_train.loc[:, data_train.columns != 'Fraud_reasons']
 x_valid = data_valid.loc[:, data_valid.columns != 'Fraud_reasons']
 x_valid = x_valid[x_train.columns]
 
+
 model = XGBClassifier(max_depth=7, learning_rate=0.2, n_estimators=200,
                       objective='multi:softmax', eval_metric='mlogloss')
-model.fit(x_train, y_train, eval_set=[(x_valid, y_valid)], early_stopping_rounds=3)
+# model = pickle.load(open("xgb.model", "rb"))
+
+model.fit(x_train, y_train, eval_set=[(x_valid, y_valid)], early_stopping_rounds=10)
 pickle.dump(model, open("xgb.model", "wb"))
 model = pickle.load(open("xgb.model", "rb"))
 
@@ -34,5 +38,11 @@ accuracy = accuracy_score(y_valid, y_pred)
 precisions = precision_score(le.inverse_transform(y_valid), le.inverse_transform(y_pred), average=None, labels=le.classes_)
 recalls = recall_score(le.inverse_transform(y_valid), le.inverse_transform(y_pred), average=None, labels=le.classes_)
 print(f'Accuracy: {100 * accuracy:.2f}%')
+
 for i in range(len(le.classes_)):
     print(f'Fraud reason: {le.classes_[i]}, Precision: {(100 * precisions[i]):.2f}%, Recall: {(100 * recalls[i]):.2f}%')
+
+fig, ax = plt.subplots(figsize=(12,18))
+xgb.plot_importance(model, max_num_features=20, height=0.5, ax=ax)
+plt.savefig('feature_importance_xgb.png', bbox_inches='tight')
+
